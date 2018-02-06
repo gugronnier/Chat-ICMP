a56cb1d8f03b5c7038ba55e4bf27e4ed555e0ab6
gugronnier <gglyon769@gmail.com>
2018-02-06 14:29:02 +0100

    update client and server

--- Client.py
+++ Client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import re
+import re,subprocess
 from scapy.all import *
 
 print "!! USE Ctrl+C FOR EXIT !!\n"
@@ -10,27 +10,40 @@ print "Demarrage Client ..."
 print "Envoi de la trame..."
 
 def filtre(pkt): # Filtre a la reception
-        data = pkt[Raw].load
-        datas = data.split(" : ")
+	global data
+	data = pkt[Raw].load
+
+def traitement(donnees):
+	datas = donnees.split(" : ")
         for key in datas:
                 # Si le message contient Serveur
                 if key == "Serveur":
                         # Affichage du message
-                        print pkt[Raw].load
-                        # Récursif, on continu la conversation
-                        main()
+                        print donnees
 
-def main():
-	# On capture le message à envoyer 
-	msg = raw_input("RATESD : ")
+def input_data():
+	msg = raw_input("CHATICMP : ")
+	return msg
+
+def envoi(msg):
 	# Envoi de la réponse
-	send( IP(dst="192.168.43.63") / ICMP(type="echo-request", id=0x123) / Raw(load="RATESD : "+msg),verbose=0)
-        # Capture de la réponse
-        sniff(prn=filtre, filter="icmp", count=1)
-        #if rcv[Raw].load != "":
-        #        print rcv[Raw].load
-        
-        
+	send( IP(dst="192.168.43.63") / ICMP(type="echo-request", id=0x123) / Raw(load="CHATICMP : " + str(msg)),verbose=0)
 
+def reception():
+	sniff(prn=filtre, filter="icmp", count=1)
+	return data
+        
+def main():
+	# Capture du message
+	msg = input_data()
+	# Envoi de la réponse
+	envoi(msg)
+	# Capture de la réponse
+	data = reception()
+	# Traitement du message
+	traitement(data)
+	# Recursif
+	main()
+
+# Initialisation 
 main()
-
--- Server.py
+++ Server.py
@@ -1,32 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+
+import re
 from scapy.all import *
-import time
 
 print "!! USE Ctrl+C FOR EXIT !!\n"
 
 print "Demarrage Serveur..."
 print "Ecoute..."
 
-def filtremessage(pkt): # Filtre a la reception
-        data = pkt[Raw].load
-        datas = data.split(" : ")
-        for key in datas:
-                # Si le message contient RATESD
-                if key == "RATESD":
-                        # Affichage du message
-                        print pkt[Raw].load
-                        # On capture le message à envoyer 
-                        msg = raw_input("Serveur : ")
-                        # On récupère l'emetteur
-                        srcip=pkt[IP].src
-                        # Envoi de la réponse
-                        send( IP(dst=srcip) / ICMP(type="echo-reply", id=0x123) / Raw(load="Serveur : $
-                        # On continu la conversation
-                        main()
+def filtre(pkt): # Filtre a la reception
+	global data,srcip
+	srcip=pkt[IP].src
+	data = pkt[Raw].load
 
-def main ():
-        # Sniff réseau des paquets ICMP
-        sniff(prn=filtremessage, filter="icmp", count=1)
+def traitement(donnees):
+	datas = donnees.split(" : ")
+	for key in datas:
+		# Si le message contient Serveur
+		if key == "CHATICMP":
+			# Affichage du message
+			print donnees
 
+def envoi(msg):
+	# Envoi de la réponse
+	send( IP(dst=srcip) / ICMP(type="echo-reply", id=0x123) / Raw(load="Serveur : " + str(msg)),verbose=0)
+
+def reception():
+	sniff(prn=filtre, filter="icmp", count=1)
+	return data
+
+def input_data():
+	msg = raw_input("Serveur : ")
+	return msg
+
+def main():
+	# Capture de la réponse
+	data = reception()
+	# Traitement du message
+	traitement(data)
+	# Capture du message a envoyer
+	msg = input_data()
+	# Envoi de la réponse
+	envoi(msg)
+	# Récursif, on continu la conversation
+	main()
+        
+
+# Initialisation 
 main()
